72841d4248fdea6e11b32429572700ffdadd553d
Prathmesh Kanekar <126949332+PrathmeshKanekar@users.noreply.github.com>
2025-10-20 00:15:52 +0530

    Enable session persistence in production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: true,
   ENCRYPT_KEY: 'prod_bank_key_replace_via_ci', // secure CI/CD only
-  PERSIST_SESSION: false,
+  PERSIST_SESSION: true,
   BASE_URL: 'https://membersap.bsite.net/api/'
 };
