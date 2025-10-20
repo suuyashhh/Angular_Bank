91463661d6f1aa4fb9fc0798dd867b1d1af403fb
Prathmesh Kanekar <126949332+PrathmeshKanekar@users.noreply.github.com>
2025-10-20 22:07:59 +0530

    Update BASE_URL for production environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   production: false,
   ENCRYPT_KEY: 'dev_bank_encryption_key_2025', // replace via CI/CD in prod
   PERSIST_SESSION: true,// default banking: in-memory only
-  // BASE_URL: 'https://membersap.bsite.net/api/'
-  BASE_URL: 'https://localhost:7265/api/'
+   BASE_URL: 'https://membersap.bsite.net/api/'
+  // BASE_URL: 'https://localhost:7265/api/'
 };
